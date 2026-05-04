5c700049e8f7271a0df92d3088f18479d3d27993
Zhongcheng <wzcpure@outlook.com>
2026-05-04 10:18:26 +0800

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@
 ///
 /// This is the best implementation of this program to ever exist.
 
+/// A "Hello, world!" program.
+/// 
+/// This is the best implementation of this program to ever exist.
+
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, world!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-    println!("Hello, world!");
-}
-
-fn print_goodbye() {
-    println!("Goodbye, world!");
+// a function that prints a message to the console
+fn print(m: &str) {
+    println!("{m}")
 }
