09312904a023948d47b1708ddd6b46b1df785f79
Zhongcheng <wzcpure@outlook.com>
2026-05-04 10:17:44 +0800

    remove goodbye message

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 /// A "Hello, world!" program.
-///
-/// This is the best implementation of this program to ever exist.
-
-/// A "Hello, world!" program.
 /// 
 /// This is the best implementation of this program to ever exist.
 
 fn main() {
     print("Hello, world!");
-    print("Goodbye, world!");
 }
 
-// a function that prints a message to the console
+// a function that prints a message
 fn print(m: &str) {
     println!("{m}")
 }
